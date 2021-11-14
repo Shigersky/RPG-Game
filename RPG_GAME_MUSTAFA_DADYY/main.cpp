022aa05074f3af2fd31d4165169d7f2243c2b128
Mateusz <bartnickimateuszgame@gmail.com>
2021-11-14 17:56:03 +0100

    V3
    Added player movement and bullets, there is an issue where the player slows down after shooting

--- RPG_GAME_MUSTAFA_DADYY/main.cpp
+++ RPG_GAME_MUSTAFA_DADYY/main.cpp
@@ -1,17 +1,35 @@
 #include <iostream >
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <math.h>
+
+//normalizing function
+sf::Vector2f NormalizeVector(sf::Vector2f vector)
+{
+    float m = sqrt(vector.x * vector.x + vector.y + vector.y);
+    sf::Vector2f normalizedVector;
+    normalizedVector.x = vector.x / m;
+    normalizedVector.y = vector.y / m;
+    return normalizedVector;
+}
 
 int main()
 {
     //---------------------------INITIALIZE-------------------------------------------------------------
-            
+
+
     //Turning On anti-aliasing
     sf::ContextSettings aa;
     aa.antialiasingLevel = 8;
 
     //Create window object from RenderWindow class, takes 2 arguments into constructor
-    sf::RenderWindow window(sf::VideoMode(800, 600), "RPG Game", sf::Style::Default, aa);
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "RPG Game", sf::Style::Default, aa);
 
+    //---------------------------INITIALIZE-------------------------------------------------------------
+
+
+
+    // --------------------------LOAD-------------------------------------------------------------------
     //Loading Player Sprite
     sf::Texture playerTexture;
     sf::Sprite playerSprite;
@@ -21,17 +39,36 @@ int main()
     playerSprite.setTexture(playerTexture);
     playerSprite.setTextureRect(sf::IntRect(64 * XIndex, 64 * YIndex, 64, 64));
     playerSprite.setScale(sf::Vector2f(2.f, 2.f));
+    playerSprite.setPosition(sf::Vector2f(20.f, 20.f));
 
-    //Movement Funcs
+    //Loading Enemy Skeleton Sprite
+    sf::Texture enemySkeletonTexture;
+    sf::Sprite enemySkeletonSprite;
+    int EXIndex = 0;
+    int EYIndex = 0;
+    float skeletonPX = 896;
+    float skeletonPY = 476;
+    enemySkeletonTexture.loadFromFile("Assets/Skeleton_Enemy/PlayerSkeleton.png");
+    enemySkeletonSprite.setTexture(enemySkeletonTexture);
+    enemySkeletonSprite.setTextureRect(sf::IntRect(64 * EXIndex, 64 * EYIndex, 64, 64));
+    enemySkeletonSprite.setPosition(sf::Vector2f(skeletonPX, skeletonPY));
+
+    // --------------------------LOAD-------------------------------------------------------------------
+
+    //Bullet
+    
+    std::vector <sf::RectangleShape> bullets;
+    float bulletSpeed = 1;
    
+    
+    
 
-    //---------------------------INITIALIZE-------------------------------------------------------------
+    //---------------------------UPDATE-------------------------------------------------------------
 
-    //Run as long as the window object isOpen, this is the main game loop
+     //Run as long as the window object isOpen, this is the main game loop
     while (window.isOpen())
     {
 
-        //---------------------------UPDATE-------------------------------------------------------------
         //event from the event class, anything that happens with the window
         sf::Event event;
 
@@ -46,53 +83,84 @@ int main()
                 window.close();
             }
 
-        } 
+        }
 
         //----------------------------UPDATE-----------------------------------------------------------
 
-           if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-            {
-              
-              
-               playerSprite.move(sf::Vector2f(0.1, 0.f));
-
-            }
-
-           if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-           {
+        //Player Movement
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        {
 
 
-               playerSprite.move(sf::Vector2f(-0.1, 0.f));
+            playerSprite.move(sf::Vector2f(0.1, 0.f));
 
-           }
+        }
 
-           if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-           {
-
-
-               playerSprite.move(sf::Vector2f(0.f, -0.1));
-
-           }
-
-           if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-           {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
 
 
-               playerSprite.move(sf::Vector2f(0.f, 0.1));
+            playerSprite.move(sf::Vector2f(-0.1, 0.f));
 
-           }
+        }
 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+        {
+
+
+            playerSprite.move(sf::Vector2f(0.f, -0.1));
+
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+        {
+
+
+            playerSprite.move(sf::Vector2f(0.f, 0.1));
+
+        }
+
+        //Bullet 
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left))
+        {
+            bullets.push_back(sf::RectangleShape(sf::Vector2f(20.f, 10.f)));
+            int bulletIndex = bullets.size() - 1;
+            sf::Vector2f playerPosition = playerSprite.getPosition();
+            sf::Vector2f bulletPosition;
+            bulletPosition.x = playerPosition.x + 64.f;
+            bulletPosition.y = playerPosition.y + 64.f;
+            bullets[bulletIndex].setPosition(bulletPosition);
+        }
+
+        for (size_t i = 0; i < bullets.size(); i++)
+        {
+           
+            sf::Vector2f bDirection = enemySkeletonSprite.getPosition() - bullets[i].getPosition();
+            bDirection = NormalizeVector(bDirection);
+            bullets[i].setPosition(bullets[i].getPosition() + bDirection * bulletSpeed);
+        }
+        
 
         //----------------------------DRAW-------------------------------------------------------------
         //clear function, uses the SFML color class
         window.clear(sf::Color::Black);
 
+
+        window.draw(enemySkeletonSprite);
         window.draw(playerSprite);
+
+            for (size_t i = 0; i < bullets.size(); i++)
+            {
+
+                window.draw(bullets[i]);
+
+            }
 
         //displays the frame in the window and ends the current frame
         window.display();
         //----------------------------DRAW------------------------------------------------------------
     }
+
 
     return 0;
 }
